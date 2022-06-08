5e27aa4d3bd3d170eecba31128262f77bf5eb976
andrescortesdev <59241395+andrescortesdev@users.noreply.github.com>
2022-06-08 18:37:47 -0500

    add videos

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
                     @if (isset($errors) && count($errors) > 0)
                         <div class="alert alert-danger">
                             <ul>
-                              <li>Correo o contraseña invalidos</li>
+                                <li>Correo o contraseña invalidos</li>
                             </ul>
                         </div>
                     @endif
@@ -23,37 +23,37 @@
                             <input class="form-control" name="email" type="email" id="email" required="">
                         </div>
                         <div class="mb-3">
-                            <div class="input-group input-group-merge">
-                                <input type="password" id="password" name="password" class="form-control">
-                                <div class="input-group-text" data-password="false">
-                                    <span class="password-eye"></span>
-                                </div>
-                            </div>
+                            <label for="password" class="form-label">Password</label><br>
+                            <input type="password" id="password" name="password" class="form-control">
                         </div>
 
-                        <div class="text-center d-grid">
-                            <button class="btn btn-primary" type="submit">Autenticarme</button>
-                        </div>
-                    </form>
-
+                <div class="text-center d-grid">
+                    <button class="btn btn-primary" type="submit">Autenticarme</button>
                 </div>
+                </form>
+
             </div>
         </div>
+    </div>
 
-        <div class="auth-fluid-right text-center">
-            <div class="auth-user-testimonial">
-                <h2 class="mb-3 text-white">FullStack Reto</h2>
-                <p class="lead"><i class="mdi mdi-format-quote-open"></i>
-                    Construya una aplicación web con estructura monolítica con Laravel que permita la gestión de
-                    cursos, estudiantes y la asignación entre estos, teniendo en cuenta que un estudiante puede estar
-                    asociado a varios cursos.
-                    <i class="mdi mdi-format-quote-close"></i>
-                </p>
-                <h5 class="text-white">
-
-                </h5>
-            </div>
+    <div class="auth-fluid-right text-center">
+        <div class="auth-user-testimonial">
+            <h2 class="mb-3 text-white">FullStack Reto</h2>
+            <p class="lead">
+                Construya una aplicación web con estructura monolítica con Laravel que permita la gestión de
+                cursos, estudiantes y la asignación entre estos, teniendo en cuenta que un estudiante puede estar
+                asociado a varios cursos.
+            </p>
+            <p class="lead">
+                usuario : test@gmail.com <br>
+                password: password <br>
+                repo : <a  target="_blank" href="https://github.com/andrescortesdev/reto-fullstack.git">https://github.com/andrescortesdev/reto-fullstack.git</a>
+            </p>
+            <video width="60%" height="auto" controls autoplay="autoplay">
+                <source src="{{ url('video/login.mp4') }}" type="video/mp4">
+            </video>
         </div>
+    </div>
 
     </div>
 
