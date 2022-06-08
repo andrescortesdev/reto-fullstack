936eef0b4de19f1191cd845a44b014568c55e8c8
andrescortesdev <59241395+andrescortesdev@users.noreply.github.com>
2022-06-08 18:38:57 -0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,6 @@ class AppServiceProvider extends ServiceProvider
     {
         if (config('app.env') === 'production') {
             \URL::forceScheme('https');
-            \URL::forceScheme('http');
         }
     }
 }
